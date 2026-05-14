59b5f8b4ae407487068e1f69301429d10dafb6c2
vibek44 <kshetrivibek@gmail.com>
2026-05-14 13:13:58 +0300

    adding login services using axios

---
+++ src/services/login.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+const baseUrl='/api/login'
+
+const login= async ( loginObject ) => {
+    const response=await axios.post(baseUrl,loginObject)
+    return response.data
+}
+
+export default { login }
