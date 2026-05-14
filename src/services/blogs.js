132cb00935e208f3c56f983c27987ce706326165
vibek44 <kshetrivibek@gmail.com>
2026-05-14 13:21:04 +0300

    adding creating services for the blog creation

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,9 +1,24 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+const setToken = (newToken) => {
+  const token=`Bearer ${newToken}`}
+
 const getAll = async() => {
   const response = await axios.get(baseUrl)
   return  response.data
 }
 
-export default { getAll }
+const create = async(newBlog) => {
+  const config={
+    headers:{ Authorization:token }
+  }
+
+  const response=await axios.post(baseUrl,newBlog,config)
+  return response.data
+  
+}
+
+
+
+export default { getAll ,setToken,create}
