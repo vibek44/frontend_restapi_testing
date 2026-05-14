e3cb6e7f261d2014781788bd1e1c5647852c334e
vibek44 <kshetrivibek@gmail.com>
2026-05-14 13:13:04 +0300

    Notification component to show messages in the app

---
+++ src/components/Notification.jsx
@@ -0,0 +1,12 @@
+const Notification = ({message}) => {
+    if(message.error){
+        return( <p className="error">{message.error}</p> )
+    }
+    if(message.success){
+        return( <p className="success">{message.success}</p> )
+    }
+    return null
+}
+
+
+export default Notification
