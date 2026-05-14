26b35d034c86b86cf9ee5f23f33cbdcf78660deb
vibek44 <kshetrivibek@gmail.com>
2026-05-14 13:19:55 +0300

    change in Blogs component ui content as per ui outlook

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,5 +1,5 @@
 import Blog from "./Blog";
 
-const Blogs = ({blogs}) => blogs.map( blog =><Blog key={blog.id} blog={blog}/>)
-
+const Blogs = ({blogs}) =>  blogs.map(blog => <Blog key={blog.id} blog={blog}/>)
+     
 export default Blogs
