7e3cdfdc8349b231d505294ed1614d5cc010be88
vibek44 <kshetrivibek@gmail.com>
2026-05-16 10:07:09 +0300

    adding new Togglable component with toggling feature

---
+++ src/components/Togglable.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+
+const Togglable = (props) => {
+    const [visible, setVisible]=useState(false)
+
+    const hideWhenVisible={display:visible ? 'none' :''  }
+    const showWhenVisible={display:visible ? '' :'none'  }
+
+    return(
+        <div>
+          <div style={hideWhenVisible}>
+            <button onClick={()=>setVisible(!visible)}>{props.buttonLabel}</button>
+          </div>
+          <div style={showWhenVisible}>
+             {props.children}
+           <button onClick={()=>setVisible(!visible)}>cancel</button>
+          </div>
+        </div>
+    )
+}
+
+
+export default Togglable
