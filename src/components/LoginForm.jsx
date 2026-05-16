1be592c72a653310652e1cbd0fae2ea54fbc1261
vibek44 <kshetrivibek@gmail.com>
2026-05-16 09:59:37 +0300

    putting userName and password state variable within LoginForm component instead of App component

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,42 +1,40 @@
-import Blogs from "./Blogs"
-import BlogForm from "./BlogForm"
+import { useState } from "react"
 
-const LoginForm=( { handleLogin,handleLogout,user,blogs,userName,setUserName,password,setPassword})=>{
-  if (user===null){
-    return(
-      <form onSubmit={handleLogin}>
-        <div>
-          <label>
-            username
-            <input
-              type="text"
-              value={userName}
-              onChange={({ target }) => setUserName(target.value.trim())}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            password
-            <input
-              type="password"
-              value={password}
-              onChange={({ target }) => setPassword(target.value.trim())}
-            />
-          </label>
-        </div>
-        <button type="submit">login</button>
-      </form>)
+const LoginForm=( { handleLogin})=>{
+  const [userName, setUserName]=useState('')
+  const [password, setPassword]=useState('')
+
+  const handleLoginFormInput  = (e) => {
+    e.preventDefault()
+    handleLogin({userName,password})
+    setUserName('')
+    setPassword('')
   }
 
   return(
-    <div>
-          <h3>Blogs</h3>
-          <p>{user.userName} Logged In <button onClick={handleLogout}>LogOut</button></p> 
-          <BlogForm />
-          <h4>BlogList</h4>
-          <Blogs blogs={blogs}/>
-    </div>
+    <form onSubmit={handleLoginFormInput}>
+      <div>
+        <label>
+          username
+          <input
+            type="text"
+            value={userName}
+            onChange={({ target }) => setUserName(target.value.trim())}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          password
+          <input
+            type="password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value.trim())}
+          />
+        </label>
+      </div>
+      <button type="submit">login</button>
+    </form>
   )
 }
 
