2f3e996e9c9b118cc31eb13105175cafd87cd41f
vibek44 <kshetrivibek@gmail.com>
2026-05-16 10:13:05 +0300

    made component receive  handleBlogForm props function from App responsible for adding  new blogs

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -1,30 +1,29 @@
-import { use, useState } from "react"
+import {  useState } from "react"
 
-const BlogForm = () => {
+const BlogForm = ({handleBlogForm}) => {
     const [title,setTitle]=useState("")
     const [author,setAuthor]=useState("")
     const [url,setUrl]=useState("")
     const handleFormInput = (e) => {
-         e.preventDefault()
-         console.log(author,title);
+        e.preventDefault()
+        handleBlogForm({title,author,url})    
     }
     return(
-      <form  onSubmit={handleFormInput}style={{width:400}}>
+        <form  onSubmit={handleFormInput}style={{width:400}}>
         <fieldset >
-          <legend>create your favorite blog</legend>
-          <label>
-             title: <input type="text" value={title} onChange={({target})=>setTitle(target.value)}/> 
-          </label> <br />
-          <label>
-             author: <input type="text" value={author} onChange={({target})=>setAuthor(target.value)}/> 
-          </label> <br />
-          <label>
-             url: <input type="text" value={url} onChange={({target})=>setUrl(target.value)} /> 
-          </label>  <br />
-          <button type="submit">create</button>
-          <button >cancel</button>
+            <legend>create your favorite blog</legend>
+            <label>
+                title: <input type="text" value={title} onChange={({target})=>setTitle(target.value)}/> 
+            </label> <br />
+            <label>
+                author: <input type="text" value={author} onChange={({target})=>setAuthor(target.value)}/> 
+            </label> <br />
+            <label>
+                url: <input type="text" value={url} onChange={({target})=>setUrl(target.value)} /> 
+            </label>  <br />
+            <button type="submit">submit</button>
         </fieldset>
-      </form>
+        </form>
     )
 }
 
