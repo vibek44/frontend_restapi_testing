9ab6f9086ad7c10b0fd223696413c821d2ffbfac
vibek44 <kshetrivibek@gmail.com>
2026-05-16 10:05:49 +0300

    importing BlogForm and Togglable component and placing BlogForm as child component of Togglable component

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,5 +1,19 @@
 import Blog from "./Blog";
+import BlogForm from "./BlogForm";
+import Togglable from "./Togglable";
 
-const Blogs = ({blogs}) =>  blogs.map(blog => <Blog key={blog.id} blog={blog}/>)
+const Blogs = ( {user,blogs,handleBlogForm,handleLogout} ) =>{
+    return(
+        <div>
+              <h3>Blogs</h3>
+              <p>{user.userName} Logged In <button onClick={handleLogout}>LogOut</button></p> 
+              <Togglable buttonLabel='Create new blog' >
+                <BlogForm  handleBlogForm={handleBlogForm}/>
+              </Togglable>
+              <h4>BlogList</h4>
+              {blogs.map(blog => <Blog key={blog.id} blog={blog}/>)}
+        </div>
+      )
+}
      
 export default Blogs
