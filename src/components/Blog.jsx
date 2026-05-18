6a10f34bc9f506ba0d845a14bd3b6fd2e67ab5d8
vibek44 <kshetrivibek@gmail.com>
2026-05-18 11:50:05 +0300

    added User interface hide and visible feature for component ui according to users interaction button click

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} <b>By</b> {blog.author}
-  </div>  
-)
+import { useState } from 'react'
 
+const Blog = ({ blog ,handleBlogUpdate,handleRemove }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const [view,setShow]=useState(false)
+  const hideWhenView={ display:view ? 'none' : '' }
+  const showWhenView={ display:view ? '' : 'none' }
+
+  const handleLikes = (blog) => {
+    const changedBlog={ ...blog,likes:blog.likes+1 }
+    handleBlogUpdate(changedBlog)
+  }
+
+  return(
+    <div style={blogStyle}>
+      <div style={hideWhenView}>
+        {blog.title} <b>By</b> {blog.author} <button onClick={() => setShow(!view)}>view</button>
+      </div>
+      <div style={showWhenView}>
+        <p>{blog.title} by {blog.author} <button onClick={() => setShow(!view)}>hide</button></p>
+        <a href={`${blog.url}`} target="_blank">{blog.url}</a>
+        <p>likes:{blog.likes} <button onClick={() => handleLikes(blog)}>like</button> </p>
+        <p>{blog.user.userName}</p>
+        <button onClick={() => handleRemove(blog)}>remove</button>
+      </div>
+
+    </div>
+  )
+}
 export default Blog
