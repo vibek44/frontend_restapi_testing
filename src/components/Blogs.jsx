ebf1a5589724eb2c94c7fc96859b8bcb58dafbaf
vibek44 <kshetrivibek@gmail.com>
2026-05-18 11:47:48 +0300

    extracted and passed the new props function to child component Blog

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,19 +1,19 @@
-import Blog from "./Blog";
-import BlogForm from "./BlogForm";
-import Togglable from "./Togglable";
+import Blog from './Blog'
+import BlogForm from './BlogForm'
+import Togglable from './Togglable'
 
-const Blogs = ( {user,blogs,handleBlogForm,handleLogout} ) =>{
-    return(
-        <div>
-              <h3>Blogs</h3>
-              <p>{user.userName} Logged In <button onClick={handleLogout}>LogOut</button></p> 
-              <Togglable buttonLabel='Create new blog' >
-                <BlogForm  handleBlogForm={handleBlogForm}/>
-              </Togglable>
-              <h4>BlogList</h4>
-              {blogs.map(blog => <Blog key={blog.id} blog={blog}/>)}
-        </div>
-      )
+const Blogs = ( { user,sortedBlogs,handleBlogForm,handleLogout,ref,handleBlogUpdate,handleRemove } ) => {
+  return(
+    <div>
+      <h3>Blogs</h3>
+      <p>{user.userName} Logged In <button onClick={handleLogout}>LogOut</button></p>
+      <Togglable buttonLabel='Create new blog' ref={ref}>
+        <BlogForm  handleBlogForm={handleBlogForm}/>
+      </Togglable>
+      <h4>BlogList</h4>
+      {sortedBlogs.map(blog => <Blog key={blog.id} blog={blog} handleBlogUpdate={handleBlogUpdate} handleRemove={handleRemove}/>)}
+    </div>
+  )
 }
-     
+
 export default Blogs
