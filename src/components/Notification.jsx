8585f7f563bfe24993165ba0028e00c60161b238
vibek44 <kshetrivibek@gmail.com>
2026-05-18 11:39:29 +0300

    fixed eslint indentation rules

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,11 +1,11 @@
-const Notification = ({message}) => {
-    if(message.error){
-        return( <p className="error">{message.error}</p> )
-    }
-    if(message.success){
-        return( <p className="success">{message.success}</p> )
-    }
-    return null
+const Notification = ({ message }) => {
+  if(message.error){
+    return( <p className="error">{message.error}</p> )
+  }
+  if(message.success){
+    return( <p className="success">{message.success}</p> )
+  }
+  return null
 }
 
 
