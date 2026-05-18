7c6fd0bbe9edcc5586340ba5c2f5c9fc3d773244
vibek44 <kshetrivibek@gmail.com>
2026-05-18 11:52:40 +0300

    removed fieldset and legend tag to change component UI appearance

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -1,30 +1,27 @@
-import {  useState } from "react"
+import {  useState } from 'react'
 
-const BlogForm = ({handleBlogForm}) => {
-    const [title,setTitle]=useState("")
-    const [author,setAuthor]=useState("")
-    const [url,setUrl]=useState("")
-    const handleFormInput = (e) => {
-        e.preventDefault()
-        handleBlogForm({title,author,url})    
-    }
-    return(
-        <form  onSubmit={handleFormInput}style={{width:400}}>
-        <fieldset >
-            <legend>create your favorite blog</legend>
-            <label>
-                title: <input type="text" value={title} onChange={({target})=>setTitle(target.value)}/> 
-            </label> <br />
-            <label>
-                author: <input type="text" value={author} onChange={({target})=>setAuthor(target.value)}/> 
-            </label> <br />
-            <label>
-                url: <input type="text" value={url} onChange={({target})=>setUrl(target.value)} /> 
-            </label>  <br />
-            <button type="submit">submit</button>
-        </fieldset>
-        </form>
-    )
+const BlogForm = ({ handleBlogForm }) => {
+  const [title,setTitle]=useState('')
+  const [author,setAuthor]=useState('')
+  const [url,setUrl]=useState('')
+  const handleFormInput = (e) => {
+    e.preventDefault()
+    handleBlogForm({ title,author,url })
+  }
+  return(
+    <form  onSubmit={handleFormInput}style={{ width:400 }}>
+      <label>
+            title: <input type="text" value={title} onChange={({ target }) => setTitle(target.value)}/>
+      </label> <br />
+      <label>
+            author: <input type="text" value={author} onChange={({ target }) => setAuthor(target.value)}/>
+      </label> <br />
+      <label>
+            url: <input type="text" value={url} onChange={({ target }) => setUrl(target.value)} />
+      </label>  <br />
+      <button type="submit">submit</button>
+    </form>
+  )
 }
 
 export default BlogForm
