cc8da138193033fe656b84ef22f17b5d3b9adefc
vibek44 <kshetrivibek@gmail.com>
2026-05-23 13:38:07 +0300

    fixing spaces to make code clear

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -3,7 +3,8 @@ const baseUrl = '/api/blogs'
 
 let token=null
 const setToken = (newToken) => {
-  token=`Bearer ${newToken}`}
+  token=`Bearer ${newToken}`
+}
 
 const getAll = async() => {
   const response = await axios.get(baseUrl)
