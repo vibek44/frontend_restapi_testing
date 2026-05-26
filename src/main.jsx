df3e2e02e8476422fbf66e6cae92aec644aae8e3
vibek44 <kshetrivibek@gmail.com>
2026-05-26 09:20:13 +0300

    installed material UI as run time dependency for creating UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
-import './styles/notification.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
